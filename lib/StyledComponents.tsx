30aec32051575385ab0d45e71b74ace16f952ca5
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-05-22 23:50:54 +0900

    StyledComponentのAtomicを統一

--- lib/StyledComponents.tsx
+++ lib/StyledComponents.tsx
@@ -1,13 +1,12 @@
-import { FC } from 'react'
-
-interface Styles {
-    styles: string
-}
-
-const PreStyledSquare: FC<Styles> = ({ styles }) => ( <div className={`absolute -z-10 w-wscreen/4 h-wscreen/4 ${styles}`}></div> )
-const RichBlueSquare: FC<Styles> = ({ styles }) => (<PreStyledSquare styles={`bg-prime-blue-rich ${styles}`}/>)
-const MutedBlueSquare: FC<Styles> = ({ styles }) => (<PreStyledSquare styles={`bg-prime-blue-muted ${styles}`}/>)
-export const RichBlueTLSquare = () => (<RichBlueSquare styles='left-0 top-0'/>)
-export const RichBlueBRSquare = () => (<RichBlueSquare styles='right-0 bottom-0'/>)
-export const MutedBlueTLSquare = () => (<MutedBlueSquare styles='left-0 top-0'/>)
-export const MutedBlueBRSquare = () => (<MutedBlueSquare styles='right-0 bottom-0'/>)
+export const RichBlueTLSquare = () => (
+    <div className='absolute -z-10 w-wscreen/4 h-wscreen/4 bg-prime-blue-rich left-0 top-0'></div>
+)
+export const RichBlueBRSquare = () => (
+    <div className='absolute -z-10 w-wscreen/4 h-wscreen/4 bg-prime-blue-rich right-0 bottom-0'></div>
+)
+export const MutedBlueTLSquare = () => (
+    <div className='absolute -z-10 w-wscreen/4 h-wscreen/4 bg-prime-blue-muted left-0 top-0'></div>
+)
+export const MutedBlueBRSquare = () => (
+    <div className='absolute -z-10 w-wscreen/4 h-wscreen/4 bg-prime-blue-muted right-0 bottom-0'></div>
+)
