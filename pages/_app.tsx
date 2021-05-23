47bed042a8b7870136101ee8238a0f7df92f994f
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-05-23 14:51:54 +0900

    Headに画像と名前を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <>
             <Head>
-                <link rel='one doctor' href='/images/favicon.iso'/>
+                <link rel='one doctor logo' href='/images/favicon.iso'/>
+                <title>one doctor</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <ScreenWidthContext.Provider value={isPageSmall}>
