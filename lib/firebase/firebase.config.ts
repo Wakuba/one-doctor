0a6f8bf6d1248ff2fd2dea9ee71bf0e54850f8d0
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-07 17:43:38 +0900

    deploy

--- lib/firebase/firebase.config.ts
+++ lib/firebase/firebase.config.ts
@@ -2,18 +2,22 @@ import firebase from 'firebase'
 import '@firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKqv-fpcxMh8ym-bPjzunnUVzXN2t1HBc",
-    authDomain: "onedoctor-fed5a.firebaseapp.com",
-    projectId: "onedoctor-fed5a",
-    storageBucket: "onedoctor-fed5a.appspot.com",
-    messagingSenderId: "89272473709",
-    appId: "1:89272473709:web:468d62ae095fb458744764"
-  };
+  apiKey: "AIzaSyAKqv-fpcxMh8ym-bPjzunnUVzXN2t1HBc",
+  authDomain: "onedoctor-fed5a.firebaseapp.com",
+  projectId: "onedoctor-fed5a",
+  storageBucket: "onedoctor-fed5a.appspot.com",
+  messagingSenderId: "89272473709",
+  appId: "1:89272473709:web:468d62ae095fb458744764"
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 } else {
   firebase.app();
-} 
-  
-export const db = firebase.firestore();
+}
+
+const db = firebase.firestore();
+
+module.exports = {
+  db, firebase
+}
--- pages/Inqueries.tsx
+++ pages/Inqueries.tsx
@@ -1,50 +1,71 @@
 import Header from '../components/organisms/Header'
 import Footer from '../components/organisms/Footer'
 import ContactButtonModal from '../components/molecules/ContactButtonModal'
+import { useState } from 'react'
+
+const firebaseConfig = require('../lib/firebase/firebase.config')
+const firebase = firebaseConfig.firebase
+
+
 
 const Inqueries = () => {
-  return(
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [content, setContent] = useState<string>('')
+
+  let data = { name, email, content }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    let sendMail = firebase.functions().httpsCallable('sendMail');
+    sendMail(data)
+    setName('')
+    setEmail('')
+    setContent('')
+  }
+
+  return (
     <>
       <Header />
       <div className='flex-col justify-center content-center bg-prime-blue-muted w-full'>
         <main className='w-full flex flex-col items-center'>
-          <form action='' className='sm:w-11/12 ov-md:w-8/12 space-y-10'>
+          <form action='' onSubmit={onSubmit} className='sm:w-11/12 ov-md:w-8/12 space-y-10'>
             <div className='mt-10'>
-                <h1 className='text-2xl text-prime-blue-rich font-semibold mb-6'>お問い合わせ</h1>
-                <p className='text-sm'>以下のフォームに必要事項をご記入のうえ、「送信する」をクリックしてください</p>
+              <h1 className='text-2xl text-prime-blue-rich font-semibold mb-6'>お問い合わせ</h1>
+              <p className='text-sm'>以下のフォームに必要事項をご記入のうえ、「送信する」をクリックしてください</p>
             </div>
 
             <div>
               <label>
                 <p className='text-sm'>お名前(必須)</p>
-                <input className='w-full border-2 rounded' type='text' name='name'></input>
+                <input value={name} onChange={e => { setName(e.target.value) }} className='w-full border-2 rounded' type='text' name='name'></input>
               </label>
             </div>
 
             <div>
               <label>
                 <p className='text-sm'>メールアドレス(必須)</p>
-                <input className='w-full border-2 rounded' type='mail-adress' name='name'></input>
+                <input value={email} onChange={e => setEmail(e.target.value)} className='w-full border-2 rounded' type='mail-adress' name='name'></input>
               </label>
             </div>
 
             <div >
               <label>
                 <p className='text-sm'>お問い合わせ内容(必須)</p>
-                <textarea cols={40} rows={8} name='content' className='w-full border-2 rounded'></textarea>
+                <textarea value={content} onChange={e => { setContent(e.target.value) }} cols={40} rows={8} name='content' className='w-full border-2 rounded'></textarea>
               </label>
             </div>
 
             <div className='flex justify-center pb-10'>
-              <button className='rounded text-white h-11 w-48 bg-prime-blue-rich'>送信する</button>
+              <button type='submit' className='rounded text-white h-11 w-48 bg-prime-blue-rich'>送信する</button>
             </div>
           </form >
-          <ContactButtonModal/>
+          <ContactButtonModal />
         </main>
       </div>
-      <Footer/>
-  </>
-)
+      <Footer />
+    </>
+  )
 }
 
 export default Inqueries
