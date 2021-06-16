2de15f23a873e730aed718d2c33cfc3d94850a05
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-16 20:32:50 +0900

    firebase api keyのローカル管理化

--- src/lib/firebase/firebase.config.ts
+++ src/lib/firebase/firebase.config.ts
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 import '@firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKqv-fpcxMh8ym-bPjzunnUVzXN2t1HBc",
-  authDomain: "onedoctor-fed5a.firebaseapp.com",
-  projectId: "onedoctor-fed5a",
-  storageBucket: "onedoctor-fed5a.appspot.com",
-  messagingSenderId: "89272473709",
-  appId: "1:89272473709:web:468d62ae095fb458744764"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
