59cc43a7ed3b1cf9521643a7a642582555dde579
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-17 18:02:35 +0900

    emailjsの実装

--- src/pages/Inqueries.tsx
+++ src/pages/Inqueries.tsx
@@ -1,13 +1,26 @@
 import Header from '../components/organisms/Header'
 import Footer from '../components/organisms/Footer'
 import ContactButtonModal from '../components/molecules/ContactButtonModal'
+import React from 'react'
+import emailjs from 'emailjs-com'
+
 export default function Inqueries() {
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm(process.env.EMAIL_JS_SERVICE, process.env.TEMPLATE_ID, e.target, process.env.USER_ID)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
   return (
     <>
       <Header />
       <div className='flex-col justify-center content-center bg-prime-blue-muted w-full'>
         <main className='w-full flex flex-col items-center'>
-          <form action='' className='sm:w-11/12 ov-md:w-8/12 space-y-10'>
+          <form action='' onSubmit={sendEmail} className='sm:w-11/12 ov-md:w-8/12 space-y-10'>
+
             <div className='mt-10'>
               <h1 className='text-2xl text-prime-blue-rich font-semibold mb-6'>お問い合わせ</h1>
               <p className='text-sm'>以下のフォームに必要事項をご記入のうえ、「送信する」をクリックしてください</p>
@@ -16,21 +29,21 @@ export default function Inqueries() {
             <div>
               <label>
                 <p className='text-sm'>お名前(必須)</p>
-                <input className='w-full border-2 rounded' type='text' name='name'></input>
+                <input className='w-full border-2 rounded' type='text' name='user_name'></input>
               </label>
             </div>
 
             <div>
               <label>
                 <p className='text-sm'>メールアドレス(必須)</p>
-                <input className='w-full border-2 rounded' type='mail-adress' name='name'></input>
+                <input className='w-full border-2 rounded' type='email' name='user_email'></input>
               </label>
             </div>
 
             <div >
               <label>
                 <p className='text-sm'>お問い合わせ内容(必須)</p>
-                <textarea cols={40} rows={8} name='content' className='w-full border-2 rounded'></textarea>
+                <textarea cols={40} rows={8} name='message' className='w-full border-2 rounded'></textarea>
               </label>
             </div>
 
