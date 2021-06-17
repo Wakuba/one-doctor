164efa55defdac07483620424ccf005148a843ac
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-17 15:44:58 +0900

    firebaseの接続悪いので一旦削除

--- src/components/organisms/NewsBoard.tsx
+++ src/components/organisms/NewsBoard.tsx
@@ -1,19 +1,23 @@
-import { NewsLineType } from '../../lib/types'
 import NewsLine from '../molecules/NewsLine'
 
 type NewsBoardProps = {
-  content?: NewsLineType[];
+  // content?: NewsLineType[];
   layoutStyles?: any;
 }
 
-export default function NewsBoard({ content, layoutStyles }: NewsBoardProps) {
-  if (content == undefined) content = [{ article: '特になし', title: '特になし' }]
+export default function NewsBoard({ layoutStyles }: NewsBoardProps) {
+  // if (content == undefined) content = [{ article: '特になし', title: '特になし' }]
   return (
     <div className={`${layoutStyles.container}`}>
       <div className={`text-2xl sm:text-prime-blue-rich font-semibold ${layoutStyles.title}`}>NEWS</div>
       <p className='text-sm'>イベントや説明会の情報をお知らせします。</p>
       <div className='flex-col items-start bg-white rounded shadow-lg p-2.5 space-y-2'>
-        {content.map((news: any, idx: number) => <NewsLine key={idx + 1} title={news.title} >{news.article}</NewsLine>)}
+        <NewsLine key={0} title='イベント情報' >放射線科がイベントを更新しました。チェック！</NewsLine>
+        <NewsLine key={1} title='HP更新情報' >循環器内科が公式HPをリニューアルしました。</NewsLine>
+        <NewsLine key={2} title='イベント情報' >循環器内科がイベントを更新しました。チェック！</NewsLine>
+        <NewsLine key={3} title='イベント情報' >循環器内科がイベントを更新しました。チェック！</NewsLine>
+        <NewsLine key={4} title='イベント情報' >循環器内科がイベントを更新しました。チェック！</NewsLine>
+        <NewsLine key={5} title='イベント情報' >循環器内科がイベントを更新しました。チェック！</NewsLine>
       </div>
     </div>
   )
