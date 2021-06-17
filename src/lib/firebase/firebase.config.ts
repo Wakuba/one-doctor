775159d3197a270053d74b611aac612c0f2f0e7c
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-17 22:46:12 +0900

    firebaseのconfigでapikeyがクライアント側で参照できるようにした

--- src/lib/firebase/firebase.config.ts
+++ src/lib/firebase/firebase.config.ts
@@ -1,15 +1,14 @@
 import firebase from 'firebase/app'
 import '@firebase/firestore'
-import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  apiKey: process.env.BROWSER_FIREBASE_API_KEY,
+  authDomain: process.env.BROWSER_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.BROWSER_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.BROWSER_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.BROWSER_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.BROWSER_FIREBASE_APP_ID,
+  databaseURL: process.env.BROWSER_FIREBASE_DATABASE_URL,
 };
 
 if (!firebase.apps.length) {
