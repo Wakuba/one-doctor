2c6409bd87b78f16953f52c75dc6afbf007e780c
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-17 22:44:23 +0900

    ProcessEnvの型定義を拡張するべくdeclarations.d.tsを作成

---
+++ declarations.d.ts
@@ -0,0 +1,11 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    readonly FIREBASE_API_KEY: string
+    readonly FIREBASE_AUTH_DOMAIN: string
+    readonly FIREBASE_PROJECT_ID: string
+    readonly FIREBASE_STORAGE_BUCKET: string
+    readonly FIREBASE_MESSAGING_SENDER_ID: string
+    readonly FIREBASE_APP_ID: string
+    readonly FIREBASE_DATABASE_URL: stirng
+  }
+}
