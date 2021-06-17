ca84e861c85679d28b8e7ba66dc18a307d0e13e3
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-17 15:40:04 +0900

    apikeyの変数名を変更。databaseUrlを追加

--- src/lib/firebase/firebase.config.ts
+++ src/lib/firebase/firebase.config.ts
@@ -1,13 +1,15 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import '@firebase/firestore'
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 };
 
 if (!firebase.apps.length) {
