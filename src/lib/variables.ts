e9f29f69452bd03f7491902e01bb47e74008ecba
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-18 11:52:58 +0900

    不要なファイルを削除

---
+++ src/lib/variables.ts
@@ -0,0 +1,5 @@
+export const YOUTUBE_VIDEOLIST_API= 'https://www.googleapis.com/youtube/v3/videos' 
+
+export const WIDTH_THRESHOLD: number=767 
+export const mainBlueRich = '#5DB0D0'
+export const mainBlueMuted = '#F8FDFF'
