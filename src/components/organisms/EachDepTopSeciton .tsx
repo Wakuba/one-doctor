a080ba20a2001c5f6fbc638869d080b1bb546204
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-25 10:35:00 +0900

    診療科ページのstateの関わるセクションを切り分け

---
+++ src/components/organisms/EachDepTopSeciton .tsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import ThreePointLeader from '../atoms/ThreePointLeader'
+import { ModalMainArea, ModalBackdrop } from '../atoms/Modal'
+
+const PushPoint = (props: { children: JSX.Element[] }) => (
+  <div>
+    <div className='font-semibold text-base' >{props.children[0]}</div>
+    <div className='text-sm'>{props.children[1]}</div>
+  </div>
+)
+
+export default function EachDepTopSection({ layoutStyle }: { layoutStyle: string }) {
+  const [isTextModalOpen, setIsTextModalOpen] = useState<boolean>(false)
+  return (
+
+    <section className={`${layoutStyle} relative w-full flex flex-col items-center `} >
+      <div onClick={() => setIsTextModalOpen(true)} className='sm:w-11/12 ov-md:w-8/12 ov-md:h-96  bg-white flex sm:flex-col ov-md:flex-row shadow-md'>
+        <div className='ov-md:p-8 ov-md:flex-1'>
+          <div className='sm:p-3 sm:h-72  overflow-y-hidden relative'>
+            <div className='space-y-4 ov-md:h-72 '>
+              <div className='text-prime-blue-rich border-prime-blue-rich font-semibold border-b-2 text-base'>
+                筑波大学附属病院　循環器内科イチオシポイント
+              </div>
+              <PushPoint>
+                <span>教育：</span>
+                <span>テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキストテキストテキトスステキストテキストテキストテキストテキステ</span>
+              </PushPoint>
+              <PushPoint>
+                <span>臨床：</span>
+                <span>テテキストテキストテキストテキストキテストテキストテキストテキストテキストテキストキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテ</span>
+              </PushPoint>
+              <PushPoint>
+                <span>研究：</span>
+                <span>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキストテキストテキトスステキストテキストテキストテキストテキスト</span>
+              </PushPoint>
+            </div>
+            <div className='absolute w-full left-0 bottom-0 shadow-for-readmore h-16'></div>
+          </div>
+          <ThreePointLeader />
+          <button className='w-full h-8 text-xs bg-white focus:outline-none'>もっと読む</button>
+        </div>
+        <div className='relative ov-md:flex-1 ov-md:flex ov-md:items-start w-full h-full'>
+          <div className='border-0
+                              bg-transparent
+                              absolute
+                              z-20
+                              sm:shadow-for-narrow-ichioshi-img
+                              sm:h-24
+                              sm:w-full
+                              ov-md:shadow-for-wide-ichioshi-img
+                              ov-md:h-full
+                              ov-md:w-24
+                              ' ></div>
+          <img className='sm:w-full sm:h-auto ov-md:h-full ov-md:object-cover z-10 border-white' src='/images/ichioshi-image.png' />
+
+        </div>
+      </div>
+      {isTextModalOpen &&
+        <>
+          <ModalMainArea closeModal={() => setIsTextModalOpen(false)} modalWrapperStyle='sm:w-9/12 ov-md:w-wscreen7/10 h-5/6' modalContainerStyle='w-full'>
+            <div className='text-prime-blue-rich border-prime-blue-rich font-semibold border-b-2 text-base'>
+              筑波大学附属病院　循環器内科イチオシポイント
+            </div>
+            <PushPoint>
+              <span>教育：</span>
+              <span>
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+                テキストテキストテキストテキストテキトスステキストテキストテキストテキストテキストテキストテキストテキス
+              </span>
+            </PushPoint>
+            <PushPoint>
+              <span>臨床：</span>
+              <span>
+                テテキストテキストテキストテキストキテストテキストテキストテキストテキストテキストキストテキストテキスト
+                テテキストテキストテキストテキストキテストテキストテキストテキストテキストテキストキストテキストテキスト
+                テテキストテキストテキストテキストキテストテキストテキストテキストテキストテキストキストテキストテキスト
+                テテキストテキストテキストテキストキテストテキストテキストテキストテキストテキストキストテキストテキスト
+                テテキストテキストテキストテキストキテストテキストテキストテキストテキストテキストキストテキストテキスト
+                テテキストテキストテキストテキストキテストテキストテキストテキストテキストテキストキストテキストテキスト
+              </span>
+            </PushPoint>
+            <PushPoint>
+              <span>研究：</span>
+              <span>
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </span>
+            </PushPoint>
+          </ModalMainArea>
+          <ModalBackdrop closeModal={() => setIsTextModalOpen(false)} />
+        </>
+      }
+    </section>
+  )
+}
