0edffb4cbe42397eca622d28ae240eb1113a4fbd
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-27 22:25:49 +0900

    Headerの画像のパス修正

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className={`sm:static sm:top-0 sm:bg-white ov-md:fixed ov-md:bg-transparent py-1.5 sm:h-20 ov-md:min-h-20  ov-md:h-wscreen/20 w-full flex-row z-50`}>
       <Link href='/' >
         <div className='h-14'>
-          <img className='object-contain h-12' src='images/onedoctor.png' alt='one doctor logo' />
+          <img className='object-contain h-12' src='/images/onedoctor.png' alt='one doctor logo' />
           <div className='text-xs pl-2 sm:text-prime-blue-rich ov-md:text-white'>筑波大学特設サイト</div>
         </div>
       </Link>
