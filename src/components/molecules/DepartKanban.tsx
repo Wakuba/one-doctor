8a92aca740729dcd48dbd0cb7c986ed11f0256a8
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-06-28 12:48:48 +0900

    診療科カンバンの画像サイズ調整

--- src/components/molecules/DepartKanban.tsx
+++ src/components/molecules/DepartKanban.tsx
@@ -13,7 +13,9 @@ export default function DepartKanban({ children, layoutStyle, correspondedPage }
     <Link href={correspondedPage}>
       <button className={`
       ${layoutStyle}
-        bg-white rounded shadow-xl flex flex-row items-center sm:h-14 sm:w-52 md:h-16 md:w-64
+        bg-white rounded shadow-xl flex flex-row items-center
+        sm:h-14 sm:w-52
+        md:h-16 md:w-64
         lg:h-wscreen/20 lg:w-wscreen/5 lg:max-h-16 lg:max-w-64
         ov-xl:h-16 ov-xl:w-64
        `}>
@@ -21,7 +23,7 @@ export default function DepartKanban({ children, layoutStyle, correspondedPage }
         sm:h-11 sm:w-11
         md:h-12 md:w-12
         lg:h-3.5vw lg:w-3.5vw lg:max-h-14 lg:max-w-14
-        ov-xl:h-14 ov-xl:w-14
+        ov-xl:h-12 ov-xl:w-12
         ' />
         <div className='sm:text-xs md:text-sm lg:text-1vw ov-xl:text-sm mr-4 '>{children}</div>
       </button>
