dd39a06fe1c3589ec442edfb4c87937dcd1fef0f
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-07-13 00:03:44 +0900

    dynamic routingのデータがテンプレートに注入できていることを確認

--- src/components/organisms/DepTopSeciton .tsx
+++ src/components/organisms/DepTopSeciton .tsx
@@ -1,18 +1,42 @@
-import { useState, ReactNode } from 'react'
+import { useState } from 'react'
 import ThreePointLeader from '../atoms/ThreePointLeader'
 import { ModalMainArea, ModalBackdrop } from '../atoms/Modal'
 import PushPoint from '../atoms/PushPoint'
+import Image from 'next/image'
 
 interface DepTopSectionPropsType {
-  layoutStyle: string;
   depName: string;
-  children: any;
+  educationalPoint: string;
+  clinicalPoint: string;
+  researchPoint: string;
+  otherPoint: string;
 }
-export default function DepTopSection({ layoutStyle, depName, children }: DepTopSectionPropsType) {
+export default function DepTopSection({ depName, educationalPoint, clinicalPoint, researchPoint, otherPoint }: DepTopSectionPropsType) {
   const [isTextModalOpen, setIsTextModalOpen] = useState<boolean>(false)
-  return (
 
-    <section className={`${layoutStyle} relative w-full flex flex-col items-center `} >
+  const PushPoints = () => (
+    <>
+      <PushPoint>
+        <span>教育：</span>
+        <span>{educationalPoint}</span>
+      </PushPoint>
+      <PushPoint>
+        <span>臨床：</span>
+        <span>{clinicalPoint}</span>
+      </PushPoint>
+      <PushPoint>
+        <span>研究：</span>
+        <span>{researchPoint}</span>
+      </PushPoint>
+      <PushPoint>
+        <span>その他：</span>
+        <span>{otherPoint}</span>
+      </PushPoint>
+    </>
+  )
+
+  return (
+    <>
       <div onClick={() => setIsTextModalOpen(true)} className='sm:w-11/12 ov-md:w-8/12 ov-md:h-96  bg-white flex sm:flex-col ov-md:flex-row shadow-md'>
         <div className='ov-md:p-8 ov-md:flex-1'>
           <div className='sm:p-3 sm:h-72  overflow-y-hidden relative'>
@@ -20,7 +44,7 @@ export default function DepTopSection({ layoutStyle, depName, children }: DepTop
               <div className='text-prime-blue-rich border-prime-blue-rich font-semibold border-b-2 text-base'>
                 筑波大学附属病院　{depName}イチオシポイント
               </div>
-              {children.map((child: JSX.Element): JSX.Element => <>{child}</>)}
+              <PushPoints />
             </div>
             <div className='absolute w-full left-0 bottom-0 shadow-for-readmore h-16'></div>
           </div>
@@ -39,62 +63,24 @@ export default function DepTopSection({ layoutStyle, depName, children }: DepTop
                               ov-md:h-full
                               ov-md:w-24
                               ' ></div>
-          <img className='sm:w-full sm:h-auto ov-md:h-full ov-md:object-cover z-10 border-white' src='/images/ichioshi-image.png' />
+          <div className='relative sm:w-full sm:h-auto ov-md:h-full ov-md:object-cover z-10 border-white'>
+            {/* <Image src='/images/ichioshi-image.png' /> */}
+          </div>
 
         </div>
       </div>
-      {isTextModalOpen &&
+      {
+        isTextModalOpen &&
         <>
           <ModalMainArea closeModal={() => setIsTextModalOpen(false)} modalWrapperStyle='sm:w-9/12 ov-md:w-wscreen7/10 h-5/6' modalContainerStyle='w-full space-y-4'>
             <div className='text-prime-blue-rich border-prime-blue-rich font-semibold border-b-2 text-base'>
               筑波大学附属病院　循環器内科イチオシポイント
             </div>
-            {children.map((child: JSX.Element): JSX.Element => <>{child}</>)}
+            <PushPoints />
           </ModalMainArea>
           <ModalBackdrop closeModal={() => setIsTextModalOpen(false)} />
         </>
       }
-    </section>
+    </>
   )
 }
-
-
-function EducationalPoint({ children }: { children: ReactNode }) {
-  return (
-    <PushPoint>
-      <span>教育：</span>
-      {children}
-    </PushPoint>
-  )
-}
-DepTopSection.EducationalPoint = EducationalPoint
-
-function ClinicalPoint({ children }: { children: ReactNode }) {
-  return (
-    <PushPoint>
-      <span>臨床：</span>
-      {children}
-    </PushPoint>
-  )
-}
-DepTopSection.ClinicalPoint = ClinicalPoint
-
-function ResearchPoint({ children }: { children: ReactNode }) {
-  return (
-    <PushPoint>
-      <span>研究：</span>
-      {children}
-    </PushPoint>
-  )
-}
-DepTopSection.ResearchPoint = ResearchPoint
-
-function OtherPoint({ children }: { children: ReactNode }) {
-  return (
-    <PushPoint>
-      <span>その他：</span>
-      {children}
-    </PushPoint>
-  )
-}
-DepTopSection.OtherPoint = OtherPoint
