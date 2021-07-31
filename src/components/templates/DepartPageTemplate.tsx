2676cc7fceaf32974126553a3c66205651a94fb1
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-07-31 21:02:38 +0900

    sliderの高さがページに収まるように

--- src/components/templates/DepartPageTemplate.tsx
+++ src/components/templates/DepartPageTemplate.tsx
@@ -104,8 +104,8 @@ export default function DepartPageTemplate({
             <AppealCardBoard />
           </section>
 
-          <section className="flex flex-col items-center mb-16">
-            <div className="w-full bg-prime-blue-rich flex flex-col items-center py-6">
+          <section className="flex flex-col items-center mb-16 max-h-[90vh]">
+            <div className="w-full bg-prime-blue-rich flex flex-col items-center py-6 ">
               <p className="text-white sm:text-xs ov-md:text-md sm:my-2 ov-md:my-4 breakAll">
                 紹介動画を視聴して雰囲気をみてみませんか？
               </p>
