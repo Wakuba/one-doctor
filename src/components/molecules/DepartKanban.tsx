26272787eee820df47cdfa2cd4eccafcf2311905
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-08-01 10:45:46 +0900

    prettier

--- src/components/molecules/DepartKanban.tsx
+++ src/components/molecules/DepartKanban.tsx
@@ -23,8 +23,8 @@ export default function DepartKanban({ children, pageUrl }: DepartKanbanProps) {
       <img
         className={clsx(
           'mx-6 bg-prime-blue-muted border-solid',
-        'sm:h-11 sm:w-11',
-        'ov-md:h-12 ov-md:w-12'
+          'sm:h-11 sm:w-11',
+          'ov-md:h-12 ov-md:w-12'
         )}
       />
       <div className='sm:text-xs md:text-sm lg:text-1vw ov-xl:text-sm mr-4 '>
