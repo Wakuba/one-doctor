58163bd82f9d714bd3237f8c1b81fbbeaec59772
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-09-03 21:59:30 +0900

    templateをpageに併合

--- src/lib/customFunctions.ts
+++ src/lib/customFunctions.ts
@@ -2,14 +2,14 @@ const patternExtractorFromIframe = `http(s)?://www.google.com/maps/embed(\\S)*(?
 const patternExtractorFromTwitterTimelineEmbedder = `http(s)?://twitter.com/(\\S)*(?=('|"))`
 
 const getUrl = (text: string, extractor: string): RegExpMatchArray | string => {
-  if (text === '') return ''
+  if (!text) return ''
   const patternExtractor = new RegExp(extractor, 'g')
-  const url = text.match(patternExtractor)
+  const url = String(text).match(patternExtractor)
   console.log(url)
   return url ? url : ''
 }
 
-export const getUrlFromIfame = (text: string): RegExpMatchArray | string =>
+export const getUrlFromIframe = (text: string): RegExpMatchArray | string =>
   getUrl(text, patternExtractorFromIframe)
 
 export const getUrlFromTwitterTimeline = (
