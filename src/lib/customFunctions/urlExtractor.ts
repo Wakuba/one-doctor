d86543f5debaab6794cc0bbdd9e66077eadc9a2b
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-10-13 19:21:01 +0900

    created customFunction directory and recreated it in urlExtractor file because other custom functions get needed

--- src/lib/customFunctions/urlExtractor.ts
+++ src/lib/customFunctions/urlExtractor.ts
@@ -4,8 +4,11 @@ const patternExtractorFromTwitterTimelineEmbedder = `http(s)?://twitter.com/(\\S
 const getUrl = (text: string, extractor: string): RegExpMatchArray | string => {
   if (!text) return ''
   const patternExtractor = new RegExp(extractor, 'g')
-  const url = String(text).match(patternExtractor)
-  return url ? url : ''
+  const url =
+    String(text).slice(0, 4) === 'http'
+      ? text
+      : String(text).match(patternExtractor)
+  return url ? url : text
 }
 
 export const getUrlFromIframe = (text: string): RegExpMatchArray | string =>
