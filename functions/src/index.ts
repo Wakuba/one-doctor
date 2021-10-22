153b1d25699e20db3c4a952de3cc7ce9c0a52575
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-10-22 17:52:13 +0900

    created new index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,7 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { expressReceiver } from "./slack/app";
+import usePostMessage from "./slack/postMessage";
+
+export const slack = functions.https.onRequest(expressReceiver.app);
+export const postMessageToSlackChannelWithUserData = functions.https.onCall(() => usePostMessage());
