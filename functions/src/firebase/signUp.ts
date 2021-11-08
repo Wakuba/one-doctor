d14fc93e3a45340a167c1ed070d8f9e430ec0ea2
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-11-08 16:40:15 +0900

    created signUp and actionListener

---
+++ functions/src/firebase/signUp.ts
@@ -0,0 +1,13 @@
+import { createUserWithEmailAndPassword } from "@firebase/auth";
+import { auth } from "./firebaseConfig"
+
+interface SignupType {
+	email: string
+	password: string
+}
+
+const signUp = async ({ email, password }: SignupType): Promise<void> => {
+	await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export default signUp
