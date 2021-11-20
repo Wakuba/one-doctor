8ac4910409c2e316c980360ea9e952e4edc8595a
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-11-20 18:16:56 +0900

    deleted customFunctions.ts file and created customFunctions directory tree. Moreover, transplanted functions on firebase cloud functions to this directory

---
+++ src/lib/customFunctions/createMessageBlock.ts
@@ -0,0 +1,59 @@
+/* eslint-disable prettier/prettier */
+import { SignUpAuthorizationDataWithImageUrl } from '../types'
+
+//  ユーザー確認メッセージを表示するBlockを作成
+export default function createMessageBlock (
+  data: SignUpAuthorizationDataWithImageUrl
+) {
+  const { name, email, password, certificationImageUrl } = data
+  return [
+    {
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": name ? name : "No name",
+      },
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "plain_text",
+        "text":  email ?? "No email address",
+      },
+      "block_id": "text1",
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "plain_text",
+        "text": password ?? "No password",
+      },
+      "block_id": "text2",
+    },
+    {
+      "type": "image",
+      "title": {
+        "type": "plain_text",
+        "text": "医学生および研修医の証明となるもの",
+        "emoji": true,
+      },
+      "image_url": certificationImageUrl ?? "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+      "alt_text": "cute cat",
+    },
+    {
+      "type": "actions",
+      "block_id": "is_approved_or_not",
+      "elements": [
+        {
+          "type": "button",
+          "action_id": "is_medcoworker",
+          "text": {
+            "type": "plain_text",
+            "text": "認可",
+          },
+          "style": "primary",
+        },
+      ],
+    },
+  ];
+};
---
+++ src/lib/customFunctions/imageUploader.ts
@@ -0,0 +1,40 @@
+import { addDoc, collection } from '@firebase/firestore'
+import imageCompression from 'browser-image-compression'
+import { ref, uploadBytesResumable } from 'firebase/storage'
+import { db, storage } from '../firebase/firebase.config'
+
+const imageUploader = async (certificationImage: File) => {
+  const metadata = {
+    contentType: '',
+    customMetadata: {
+      certificationImageId: '',
+    },
+  }
+  await addDoc(collection(db, 'odAuthorizationImageFiles'), {
+    certificationlmageName: certificationImage?.name,
+  }).then((res) => {
+    console.log('addDocRes', res)
+    metadata.customMetadata.certificationImageId = res.id
+  })
+
+  const storageRef = ref(
+    storage,
+    `odUser/certificationImage/${certificationImage?.name}`
+  )
+
+  await imageCompression(certificationImage, { maxSizeMB: 3 }).then((image) => {
+    metadata.contentType = image.type
+    const uploadTask = uploadBytesResumable(storageRef, image, metadata)
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const prog = Math.round(snapshot.bytesTransferred / snapshot.totalBytes)
+        console.log('image upload progress', prog)
+      },
+      (error) => alert(error)
+    )
+  })
+  return metadata.customMetadata.certificationImageId
+}
+
+export default imageUploader
