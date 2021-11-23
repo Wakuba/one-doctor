d84034fca384e9fac42c244ea4b5e662897cec55
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-11-23 20:06:23 +0900

    SimpleSelectコンポネントでcontrolが必要なためinstall

--- src/components/organisms/Form.tsx
+++ src/components/organisms/Form.tsx
@@ -19,6 +19,7 @@ const Form: VFC<FormPropsType> = ({ formName, children, onSubmit, style }) => {
   const {
     handleSubmit,
     register,
+    control,
     formState: { errors },
   } = useForm()
 
@@ -32,6 +33,7 @@ const Form: VFC<FormPropsType> = ({ formName, children, onSubmit, style }) => {
                     ...child.props,
                     register,
                     errors,
+                    control,
                     key: child.props.name,
                   },
                 })
