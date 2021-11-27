c04adde7cf71312a5c3bce359e5a67e1fb480ed2
wakuba <1insp99persptashtash16@gmail.com>
2021-11-27 21:23:21 +0900

    try catchを実装

--- src/pages/api/postFormToSlack/[formData].tsx
+++ src/pages/api/postFormToSlack/[formData].tsx
@@ -14,18 +14,21 @@ type Response = {
 export default (req: NextApiRequest, res: NextApiResponse<Response>) => {
   const formData = req.query.formData as string
   console.log('form', formData)
-  if (formData) {
-    const parsedData: FormData = JSON.parse(formData)
-    console.log('parsed', parsedData)
-    if (process.env.SLACK_FORM_RECEIVER_CHANNEL_ID) {
-      client.chat.postMessage({
-        channel: process.env.SLACK_FORM_RECEIVER_CHANNEL_ID,
-        text: parsedData.name,
-        blocks: createMessageBlockForFormReceiver(parsedData),
-      })
+  try {
+    if (formData) {
+      const parsedData: FormData = JSON.parse(formData)
+      console.log('parsed', parsedData)
+      if (process.env.SLACK_FORM_RECEIVER_CHANNEL_ID) {
+        client.chat.postMessage({
+          channel: process.env.SLACK_FORM_RECEIVER_CHANNEL_ID,
+          text: parsedData.name,
+          blocks: createMessageBlockForFormReceiver(parsedData),
+        })
+      }
+      res.status(200).json({ message: 'Post message completed' })
     }
-    res.status(200).json({ message: 'Post message completed' })
-  } else {
+  } catch (e) {
+    console.log(e)
     res.status(400).json({ message: 'No postData' })
   }
 }
