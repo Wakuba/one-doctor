f5f88491837befe4af5e6d5d19d5370e9f7f4b72
wakuba <1insp99persptashtash16@gmail.com>
2021-11-27 21:53:35 +0900

    chat.postMessageをconsole.log()

--- src/pages/api/postFormToSlack/[formData].tsx
+++ src/pages/api/postFormToSlack/[formData].tsx
@@ -22,11 +22,12 @@ export default (req: NextApiRequest, res: NextApiResponse<Response>) => {
       console.log('parsed', parsedData)
       if (process.env.SLACK_FORM_RECEIVER_CHANNEL_ID) {
         console.log(process.env.SLACK_FORM_RECEIVER_CHANNEL_ID)
-        client.chat.postMessage({
+        const result = client.chat.postMessage({
           channel: process.env.SLACK_FORM_RECEIVER_CHANNEL_ID,
           text: parsedData.name,
           blocks: createMessageBlockForFormReceiver(parsedData),
         })
+        console.log('result', result)
       }
     }
     res.status(200).json({ message: 'Post message completed' })
