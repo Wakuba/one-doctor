7f32f0619a17e5f7db55704798225948719c8b46
wakuba <1insp99persptashtash16@gmail.com>
2021-11-28 17:14:56 +0900

    postFormToSlack APIを廃止

--- functions/src/slack/postFormData.ts
+++ functions/src/slack/postFormData.ts
@@ -1,20 +1,8 @@
-// import { LogLevel, retryPolicies, WebClient } from '@slack/web-api'
 import { createMessageBlockForFormReceiver } from './createMessageBlock'
 import { FormData } from '../type'
-// import { HttpsProxyAgent } from 'https-proxy-agent'
 import { config } from '../index'
 import { app } from '../slack/app'
 
-// const proxy = new HttpsProxyAgent(
-//   process.env.http_proxy || 'http://168.63.76.32:3128'
-// )
-
-// const client = new WebClient(config.slack.token, {
-//   maxRequestConcurrency: 5,
-//   retryConfig: retryPolicies.fiveRetriesInFiveMinutes,
-//   logLevel: LogLevel.DEBUG,
-//   agent: proxy,
-// })
 
 const postFormData = (formData: FormData): void => {
   console.log('formdata', formData)
