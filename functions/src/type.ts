fb16ca349014c31984e22d7c82dbe2aaf2ee14d0
wakuba <1insp99persptashtash16@gmail.com>
2021-12-21 15:35:08 +0900

    型の整理2

--- functions/src/type.ts
+++ functions/src/type.ts
@@ -1,16 +1,45 @@
-export interface SignUpData {
-  name: string
-  email: string
-  password: string
-}
-
-export interface SignupType {
-  email: string
-  password: string
-}
-
 export interface FormData {
   name: string
   email: string
   content: string
+}
+
+export interface SignUpData {
+  name: string
+  email: string
+  password: string
+  ruby: string
+  gender: string
+}
+
+export interface SignUpDataForStudent extends SignUpData {
+  grade: string
+  university: string
+  workplaceWishFor: string[]
+  departmentWishFor: string[]
+}
+
+export interface SignUpDataForNotStudent extends SignUpData {
+  departmentWishFor: string[]
+  workplace: string
+  workplaceWishFor: string[]
+}
+
+export interface SignUpDataForNotStudentWithUid
+  extends SignUpDataForNotStudent {
+  uid: string
+}
+
+export interface SignUpDataForStudentWithUid {
+  uid: string
+}
+
+export interface SignUpAuthorizationDataWithImageId
+  extends SignUpDataForNotStudent {
+  certificationImageId: string
+}
+
+export interface SignUpAuthorizationDataWithImageUrl
+  extends SignUpDataForNotStudent {
+  certificationImageUrl: string
 }
