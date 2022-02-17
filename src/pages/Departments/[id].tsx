59ef83c96cace4f1c021b2b6a1193b2428c790e1
wakuba <1insp99persptashtash16@gmail.com>
2022-02-18 01:25:18 +0900

    GeographicalInformationTabをURLからマップ埋め込みが可能なようにした

--- src/pages/Departments/[id].tsx
+++ src/pages/Departments/[id].tsx
@@ -119,23 +119,10 @@ export default function DepartmentPage({ postData }: DepartmentPagePropsType) {
                 title='医局員'
                 crewDataList={tabMenu.crewCardListTab}
               />
-              <div
+              <GeoInfoTab
                 title='周辺地図'
-                className='w-full ov-md:p-8 sm:p-4 bg-white flex flex-col'
-              >
-                <div className='space-y-8'>
-                  <div className='border-l-8 block bg-prime-blue-muted px-2 border-prime-blue-rich sm:text-sm ov-md:text-md font-medium'>
-                    周辺地図
-                  </div>
-                  <iframe
-                    className='w-full mb-10 h-[50vw]'
-                    src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d279785.1765704249!2d140.17047807758485!3d35.991258388550875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60220bff99f57b0b%3A0x1cad40e7632fb4b8!2z562R5rOi5aSn5a2m!5e0!3m2!1sja!2sjp!4v1626441216082!5m2!1sja!2sjp'
-                    width='600'
-                    height='450'
-                    loading='lazy'
-                  ></iframe>
-                </div>
-              </div>
+                gmIframe={tabMenu.geographicalInformationTab.googleMapIframe}
+              />
               <EventTab
                 title='イベント'
                 dep={departmentName.departmentNameInJapanese}
