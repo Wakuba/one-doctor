dd5fe68221b1ec945ce5043b0378be313337da2c
wakuba <1insp99persptashtash16@gmail.com>
2022-03-27 11:32:24 +0900

    useFormPersistをカスタムに変更

---
+++ src/lib/customHooks/useFormPersist.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from 'react'
+
+const useFormPersist = (
+  name: string,
+  {
+    watch,
+    setValue,
+  }: {
+    watch: any
+    setValue: any
+  },
+  {
+    storage,
+    exclude = [],
+    include,
+    onDataRestored,
+    validate = false,
+    dirty = false,
+  }: any = {}
+) => {
+  const values = watch(include)
+  const getStorage = () => storage || window.sessionStorage
+
+  useEffect(() => {
+    const str = getStorage().getItem(name)
+    if (str) {
+      const values = JSON.parse(str)
+      const dataRestored = {}
+
+      Object.keys(values).forEach((key) => {
+        const shouldSet = !exclude.includes(key)
+        if (shouldSet) {
+          dataRestored[key] = values[key]
+          setValue(key, values[key], {
+            shouldValidate: validate,
+            shouldDirty: dirty,
+          })
+        }
+      })
+
+      if (onDataRestored) {
+        onDataRestored(dataRestored)
+      }
+    }
+  }, [name])
+
+  useEffect(() => {
+    getStorage().setItem(name, JSON.stringify(values))
+  })
+
+  return {
+    clear: () => getStorage().removeItem(name),
+  }
+}
+
+export default useFormPersist
