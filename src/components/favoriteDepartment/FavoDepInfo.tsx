7fa9bc579020b6de22c8814a916c402591b3485f
wakuba <1insp99persptashtash16@gmail.com>
2022-04-30 16:11:09 +0900

    bug fix3

--- src/components/favoriteDepartment/FavoDepInfo.tsx
+++ src/components/favoriteDepartment/FavoDepInfo.tsx
@@ -9,6 +9,7 @@ const FavoDepInfoTab: VFC<FavoDepInfoTabPropsType> = ({ title }) => {
   const { data } = useSWR(
     `https://script.google.com/macros/s/AKfycbyxgL2QQoiLg3vSFnBzm0pPkZ2-S81l1UdvBVYMcZlAfVJ3k5y5zbzSSEOQOQR6_-av/exec?dep=小児外科&univ=筑波大学`
   )
+  console.log(data)
   return (
     <div title={title}>
       <div className=''>診療科からのお知らせ</div>
